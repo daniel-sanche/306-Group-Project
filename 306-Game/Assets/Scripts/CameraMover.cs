cc24219754177c0babc4fc478d469310b114cf0b
Daniel Sanche <d.sanche14@gmail.com>
2016-09-28 00:18:09 -0600

    added script to move around camera

---
+++ 306-Game/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour {
+	/**
+	 * very simple script that lets us move the camera around with the arrow keys
+	 * Useful for early testing, but can likely be deleted later
+	 */
+
+	void Update()
+	{
+		Vector3 oldPos = this.transform.position;
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position = new Vector3(oldPos.x + 1,oldPos.y,oldPos.z);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position = new Vector3(oldPos.x -1,oldPos.y,oldPos.z);
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position = new Vector3(oldPos.x, oldPos.y-1,oldPos.z);
+		}
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position = new Vector3(oldPos.x,oldPos.y+1,oldPos.z);
+		}
+	}
+}
