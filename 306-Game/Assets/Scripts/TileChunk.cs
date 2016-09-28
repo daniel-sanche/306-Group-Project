07218705d906304fda9953d999514bca26d421f9
Daniel Sanche <d.sanche14@gmail.com>
2016-09-27 23:48:17 -0600

    refactored; setting the stage for chunking tiles together

---
+++ 306-Game/Assets/Scripts/TileChunk.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class TileChunk : MonoBehaviour {
+
+
+	private List<GameObject> tileList;
+
+	public TileChunk(List<GameObject> tiles){
+		tileList = tiles;
+	}
+
+	// Use this for initialization
+	public void Remove () {
+		foreach (GameObject thisTile in tileList) {
+			GameObject.Destroy (thisTile);
+		}
+	}
+	
+
+}
--- 306-Game/Assets/Scripts/TileGenerator.cs
+++ 306-Game/Assets/Scripts/TileGenerator.cs
@@ -6,16 +6,22 @@ using Random = UnityEngine.Random;
 [System.Serializable]
 public class TileGenerator : MonoBehaviour {
 
-	public int cols = 10;
-	public int rows = 10;
+	private Transform boardHolder; 
 
 	public GameObject grass;
 	public GameObject gravel;
-	private Transform boardHolder; 
 
-	// Use this for initialization
-	public void GenerateLevel () {
-		boardHolder = new GameObject ("BoardTiles").transform;
+	public int rows = 10;
+	public int cols = 10;
+
+	void Awake () {
+		GetChunk ();
+	}
+
+
+	public TileChunk GetChunk () {
+		boardHolder = new GameObject ("Tiles").transform;
+		List<GameObject> tileList = new List<GameObject> ();
 		for (int y = 0; y < rows; y++) {
 			for(int x=0; x<cols; x++){
 				GameObject toInstantiate;
@@ -24,10 +30,12 @@ public class TileGenerator : MonoBehaviour {
 				} else {
 					toInstantiate = gravel;
 				}
-				GameObject instance = Instantiate (toInstantiate, new Vector3 (x, y, 0), Quaternion.identity) as GameObject;
+				GameObject instance = Instantiate (toInstantiate, new Vector3(x,y,0), Quaternion.identity) as GameObject;
+				tileList.Add (instance);
 				instance.transform.SetParent (boardHolder);
 			}
 		}
+		return new TileChunk (tileList);
 	}
 		
 }
