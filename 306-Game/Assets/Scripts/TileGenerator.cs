2a352e79b974f7f997b77335b317f921c23fa0a8
Daniel Sanche <Sanche@Daniels-MacBook-Pro.local>
2016-09-27 21:45:56 -0600

    re-added files. Now generates grass

---
+++ 306-Game/Assets/Scripts/TileGenerator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+[System.Serializable]
+public class TileGenerator : MonoBehaviour {
+
+	public int cols = 10;
+	public int rows = 10;
+
+	public GameObject grass;
+
+	// Use this for initialization
+	public void GenerateLevel () {
+		for (int y = 0; y < rows; y++) {
+			for(int x=0; x<cols; x++){
+				GameObject thisTile = Instantiate (grass, new Vector3(x,y, 0), Quaternion.identity) as GameObject;
+			}
+		}
+	}
+		
+}
---
+++ 306-Game/Assets/Scripts/TileRenderer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileRenderer : MonoBehaviour {
+
+	public TileGenerator generator;
+
+	void Awake () {
+		generator = GetComponent<TileGenerator> ();
+		InitGame();
+	}
+
+	void InitGame(){
+		generator.GenerateLevel ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
