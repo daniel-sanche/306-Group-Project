4beab352a869643af90b1b39735cda4243eb7a07
raisinbran13 <brandon.m.jamieson@gmail.com>
2016-09-27 22:36:08 -0600

    Basic Inventory
    Created a basic Inventory class, Slot class, and Item class. Inventory has basic searching and sorting functions.

---
+++ 306-Game/Assets/Inventory/Item.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+	public string name;
+
+	public string tooltip;
+
+	public Sprite sprite;
+}
---
+++ 306-Game/Assets/Inventory/Slot.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slot : MonoBehaviour {
+
+	/// <summary>
+	/// Currently contained item.
+	/// </summary>
+	public Item item;
+
+	// Use this for initialization
+	void Start () {
+		item = null;
+		setSprite (null);
+	}
+
+	/// <summary>
+	/// Is the slot currently empty?
+	/// </summary>
+	/// <returns> <c>true</c> if slot is empty <c>false</c> otherwise </returns>
+	public bool isEmpty(){
+		return item == null ? true : false;		//returns true if the 
+	}
+
+		
+	/// <summary>
+	/// Removes and returns the item from the slot.
+	/// </summary>
+	/// <returns>The item.</returns>
+	public Item getItem(){
+
+		if (!isEmpty()) {						//If the slot is occupied
+			Item temp = item;
+			item = null;
+			setSprite (null);					//Sets the current sprite to null
+			return temp;						//Return the item
+		}
+		
+		return null;							//Return null otherwise
+	}
+
+
+	/// <summary>
+	/// Sets the slot's current item.
+	/// </summary>
+	/// <param name="toIns"> The item to insert. </param>
+	public void setItem(Item toIns){
+		if (isEmpty ()){						//If the slot is empty
+			item = toIns;						//Add the item
+			setSprite(toIns.sprite);			//Update sprite renderer
+		}
+	}
+
+
+	/// <summary>
+	/// Gives the sprite renderer a new sprite
+	/// </summary>
+	/// <param name="sprite">The sprite to render.</param>
+	public void setSprite(Sprite sprite){
+		GetComponent<SpriteRenderer> ().sprite = sprite;		
+	}
+}
