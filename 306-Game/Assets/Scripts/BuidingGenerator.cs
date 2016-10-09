fac375f5d61fce31c003006ac9c823bb0e4be47a
Daniel Sanche <d.sanche14@gmail.com>
2016-10-09 12:30:53 -0600

    Created function to split rooms into smaller ones, like a k-d tree

--- 306-Game/Assets/Scripts/BuidingGenerator.cs
+++ 306-Game/Assets/Scripts/BuidingGenerator.cs
@@ -1,40 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Random = UnityEngine.Random;
 
 public class BuidingGenerator : MonoBehaviour {
-
-	GameObject topFloor = (GameObject)Resources.Load("/Prefabs/top");
 
 
 	public static TileType[,] GenerateBuilding(Vector2 size){
 		TileType[,] tileMap = new TileType[(int)size.x, (int)size.y];
-		CreateBorder (tileMap, size);
+		RoomNode root = new RoomNode (size);
+		root.GenerateSubtree (0.5);
 		return tileMap;
 	}
 
-	private static void CreateBorder(TileType[,] tileMap, Vector2 size){
-		for (int x = 0; x < size.x; x++) {
-			for (int y = 0; y < size.y; y++) {
-				bool isBottom = (y == 0);
-				bool isTop = (y == (int)size.y - 1);
-				bool isLeft = (x == 0);
-				bool isRight = (x == (int)size.x - 1);
-				if (isTop) {
-					tileMap [x, y] = TileType.FloorTop;
-				} else if (isBottom) {
-					tileMap [x, y] = TileType.FloorBottom;
-				} else if (isRight) {
-					tileMap [x, y] = TileType.FloorRight;
-				} else if (isLeft) {
-					tileMap [x, y] = TileType.FloorLeft;
-				} else {
-					tileMap [x, y] = TileType.Floor;
+
+	private class RoomNode {
+		private Vector2 _size;
+		private RoomNode _firstChild;
+		private RoomNode _secondChild;
+		private bool _verticalSplit;
+		public int smallestRoomArea = 4;
+
+		public RoomNode(Vector2 roomSize){
+			_size = roomSize;
+			_verticalSplit = false;
+		}
+
+		public void GenerateSubtree(double splitProb, double vertProb=0.5){
+			int height = (int)_size.y;
+			int width = (int)_size.x;
+			int area = height * width;
+			//randomly decide whether we're going to split this room into 2. never split small rooms
+			if (Random.value <= splitProb && area > smallestRoomArea) {
+				//make it so rooms small in one dimension cannot be split in that direction
+				if (height <= 2) {
+					vertProb = 1;
+				} else if (width <= 2) {
+					vertProb = -1;
 				}
+				//calculate whether we're doing a vertical split of a horizontal one
+				Vector2 firstSize = _size;
+				Vector2 secondSize  = _size;
+				if (Random.value <= vertProb) {
+					//do a vertical split
+					//find a split point on the x axis that's not an edge
+					int splitPt = (int)(Random.value * (width - 2)) + 1;
+					//adjust the child sizes to reflect split
+					firstSize.x = splitPt;
+					secondSize.x = _size.x - splitPt;
+					_verticalSplit = true;
+				} else {
+					//do a horizontal split
+					//find a split point on the y axis that's not an edge
+					int splitPt = (int)(Random.value * (height - 2)) + 1;
+					//adjust the child sizes to reflect split
+					firstSize.y = splitPt;
+					secondSize.y = _size.x - splitPt;
+					_verticalSplit = false;
+				}
+				_firstChild = new RoomNode (firstSize);
+				_secondChild = new RoomNode (secondSize);
+				_firstChild.GenerateSubtree (0.5);
+				_secondChild.GenerateSubtree(0.5);
 			}
 		}
-		tileMap [0, 0] = TileType.FloorBL;
-		tileMap [0, (int)size.y-1] = TileType.FloorTL;
-		tileMap [(int)size.x-1, 0] = TileType.FloorBR;
-		tileMap [(int)size.x-1, (int)size.y-1] = TileType.FloorTR;
 	}
+
+
 }
