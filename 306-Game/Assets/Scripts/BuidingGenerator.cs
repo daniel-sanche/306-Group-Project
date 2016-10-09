480e1edeeb946329be91df385fac1f68abea0600
Daniel Sanche <d.sanche14@gmail.com>
2016-10-09 12:37:52 -0600

    added code to generate outer border of building

--- 306-Game/Assets/Scripts/BuidingGenerator.cs
+++ 306-Game/Assets/Scripts/BuidingGenerator.cs
@@ -6,23 +6,54 @@ public class BuidingGenerator : MonoBehaviour {
 
 
 	public static TileType[,] GenerateBuilding(Vector2 size){
-		TileType[,] tileMap = new TileType[(int)size.x, (int)size.y];
 		RoomNode root = new RoomNode (size);
 		root.GenerateSubtree (0.5);
+		return GenerateTiles (root);
+	}
+
+	private static TileType[,] GenerateTiles(RoomNode root){
+		TileType[,] tileMap = GenerateOuterWalls(root);
 		return tileMap;
 	}
 
+	private static TileType[,] GenerateOuterWalls(RoomNode root){
+		TileType[,] tileMap = new TileType[(int)root._size.x, (int)root._size.y];
+		for (int x = 0; x < root._size.x; x++) {
+			for (int y = 0; y < root._size.y; y++) {
+				bool isBottom = (y == 0);
+				bool isTop = (y == (int)root._size.y - 1);
+				bool isLeft = (x == 0);
+				bool isRight = (x == (int)root._size.x - 1);
+				if (isTop) {
+					tileMap [x, y] = TileType.FloorTop;
+				} else if (isBottom) {
+					tileMap [x, y] = TileType.FloorBottom;
+				} else if (isRight) {
+					tileMap [x, y] = TileType.FloorRight;
+				} else if (isLeft) {
+					tileMap [x, y] = TileType.FloorLeft;
+				} else {
+					tileMap [x, y] = TileType.Floor;
+				}
+			}
+		}
+		tileMap [0, 0] = TileType.FloorBL;
+		tileMap [0, (int)root._size.y-1] = TileType.FloorTL;
+		tileMap [(int)root._size.x-1, 0] = TileType.FloorBR;
+		tileMap [(int)root._size.x-1, (int)root._size.y-1] = TileType.FloorTR;
+		return tileMap;
+	}
 
 	private class RoomNode {
-		private Vector2 _size;
+		public Vector2 _size;
 		private RoomNode _firstChild;
 		private RoomNode _secondChild;
-		private bool _verticalSplit;
+		private bool _verticalSplit = false;
+		private bool _isLeaf = true;
 		public int smallestRoomArea = 4;
 
 		public RoomNode(Vector2 roomSize){
 			_size = roomSize;
-			_verticalSplit = false;
 		}
 
 		public void GenerateSubtree(double splitProb, double vertProb=0.5){
@@ -31,6 +62,7 @@ public class BuidingGenerator : MonoBehaviour {
 			int area = height * width;
 			//randomly decide whether we're going to split this room into 2. never split small rooms
 			if (Random.value <= splitProb && area > smallestRoomArea) {
+				_isLeaf = false;
 				//make it so rooms small in one dimension cannot be split in that direction
 				if (height <= 2) {
 					vertProb = 1;
