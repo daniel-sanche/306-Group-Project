f40659a860ee93820777b6fefc5a5a5161b12f7b
bmj920 <bmj920@usask.ca>
2016-10-13 00:22:09 -0600

    Renamed testing script to prevent errors
    Renamed my basic test script "Player" to "InvTestPlayer" to prevent any clashing with the actual player script.

--- 306-Game/Assets/InvTestPlayer.cs
+++ 306-Game/Assets/InvTestPlayer.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class InvTestPlayer : MonoBehaviour {
 	
 	//The speed of the player
 	public float speed;				
