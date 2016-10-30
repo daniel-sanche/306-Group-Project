f1a63065385fc00111c7e7d2a448ea8412a0b2a4
Austin Peters <austin.clarke.peters@gmail.com>
2016-10-30 15:10:09 -0600

    Implemented a script that keeps track of if it is day or night and makes the game darker if it is night

---
+++ 306-Game/Assets/DayNightSystem.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DayNightSystem : MonoBehaviour {
+
+    [SerializeField]
+    Stat day;
+    [SerializeField]
+    Stat night;
+    [SerializeField]
+    Image overLay;
+    [SerializeField]
+    float dayLength;
+    [SerializeField]
+    int numDays;
+
+    bool isDay;
+    float timeLimit;
+
+    // Use this for initialization
+    void Start()
+    {
+        isDay = true;
+        timeLimit = numDays * 2 * dayLength;
+        InvokeRepeating("ChangeTimeType", 0, dayLength);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake()
+    {
+        day.Initialize();
+        night.Initialize();
+    }
+
+    void ChangeTimeType()
+    {
+        if (isDay)
+        {
+            isDay = false;
+            overLay.GetComponent<CanvasGroup>().alpha = 0;
+        }
+        else
+        {
+            isDay = true;
+            overLay.GetComponent<CanvasGroup>().alpha = 1;
+        }
+    }
+}
