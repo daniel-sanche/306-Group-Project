d3e788ae8e006421025489f5431905e5b09c0ed9
Daniel Sanche <d.sanche14@gmail.com>
2016-10-30 22:04:01 -0600

    added context to the SpriteForCode function, so we can switch which tiles are rendered based on neighbours

--- 306-Game/Assets/Scripts/TileGenerator.cs
+++ 306-Game/Assets/Scripts/TileGenerator.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using Random = UnityEngine.Random;
 
-public enum TileType {Grass, Gravel, Water, Sand, Rock, Tree, Floor, FloorTop, FloorBottom, FloorLeft, FloorRight, FloorTL, FloorTR, FloorBL, FloorBR, FloorDoorL, FloorDoorR, FloorDoorT, FloorDoorB};
+public enum TileType {Grass, Gravel, Water, Sand, Rock, Tree, Floor, FloorTop, FloorBottom, FloorLeft, FloorRight, FloorTL, FloorTR, FloorBL, FloorBR, FloorDoorL, FloorDoorR, FloorDoorT, FloorDoorB, NULL};
 
 public class TileGenerator : MonoBehaviour {
 	/**
