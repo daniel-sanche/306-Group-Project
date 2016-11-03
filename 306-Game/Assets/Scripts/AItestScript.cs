837a6a82497a734e2c48efd56c7a7b7c0ec53ea4
raisinbran13 <brandon.m.jamieson@gmail.com>
2016-11-03 15:13:21 -0500

    Updated Merged Scene
    Added various game parts to merged scene for presentation.

---
+++ 306-Game/Assets/Scripts/AItestScript.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AItestScript : MonoBehaviour {
+
+	public float aggrorange = 30f;
+
+	public DecisionTree ai;
+	public Node node_lungedecider = new Node();
+	public Node node_moveto = new Node();
+	public Node node_stroll = new Node();
+	public Node node_lunge = new Node();
+	private Vector2 mousepos;
+	private Vector2 myVector;
+	public Rigidbody2D rb;
+	public Rigidbody2D player;
+
+	bool b = true;
+	private bool lungecd = true;
+
+	void Awake(){
+		ai = new DecisionTree ();
+
+	}
+	// Use this for initialization
+	void Start () {
+		
+		rb = GetComponent<Rigidbody2D> ();
+		BuildDecisionTree ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mousepos = new Vector2 (Camera.main.ScreenToWorldPoint( Input.mousePosition).x, Camera.main.ScreenToWorldPoint( Input.mousePosition).y);
+		myVector = new Vector2 (transform.position.x, transform.position.y);
+		ai.Search (ai.root);
+		Debug.DrawRay ( transform.position, player.transform.position - (Vector3) myVector, Color.red);
+
+
+	}
+
+	void BuildDecisionTree(){
+		ai.root.decdel = RangeCheck;
+		/*ai.root.left.actdel = wentLeft;
+		ai.root.right.actdel = wentRight;
+	*/
+		node_lungedecider.value = 25;
+		node_lungedecider.decdel = LungeDecider;
+
+		node_lunge.value = 10;
+		node_lunge.actdel = Lunge;
+
+		node_moveto.actdel = MoveTo;
+		node_moveto.value = 30;
+
+		node_stroll.value = 70;
+		node_stroll.actdel = Stroll;
+
+		ai.Insert (node_lungedecider,ai.root);
+
+		ai.Insert (node_moveto, ai.root);
+		ai.Insert (node_lunge, ai.root);
+		ai.Insert (node_stroll, ai.root);
+	
+	}
+
+	public void Stroll(){
+		print ("Strolling\n");
+
+	}
+	public void MoveTo(){
+		Debug.Log("MOVING");
+	}
+
+	public void Lunge(){
+		Debug.Log ("LUNGING");
+		lungecd = false;
+		Invoke ("LungeCD", 5);
+	}
+
+
+	public bool RangeCheck(){
+		if (Vector2.Distance(player.transform.position, myVector) < aggrorange) {
+			//Debug.Log ("In range");
+			return true;
+		}
+
+		return false;
+	}
+	public bool LungeDecider(){
+
+		if (lungecd) {
+
+			int foo = Random.Range (0, 20);
+			if (foo >10) {
+
+				return true;
+			}
+		}
+
+		return false;
+	}		
+
+	public void LungeCD(){
+		lungecd = true;
+	}
+}
---
+++ 306-Game/Assets/Scripts/DecisionTree.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Node{
+	public Node right;
+	public Node left;
+	public Decision decdel;
+	public Action actdel;
+
+	/* For Insertion*/
+	public int value;
+}
+
+public delegate bool Decision();
+public delegate void Action();
+
+
+public class DecisionTree : MonoBehaviour {
+	
+
+
+	public Node root;
+	/* useful for inserting nodes*/
+	public int mid = 50;
+	/*Constructor*/
+	public  DecisionTree(){
+	
+		root = new Node();
+		root.value = mid;
+
+
+	}
+
+	public void Insert(Node newnode, Node parent){
+		if (newnode == null || parent ==null) {
+			throw new Exception ("INSERT: newnode is null or parent is null");
+		}
+
+		if (parent.value > newnode.value) {
+			if (parent.left == null) {
+				parent.left = newnode;
+			} else {
+				Insert (newnode, parent.left);
+			}
+		}
+		else {
+			if (parent.right == null) {
+				parent.right = newnode;
+			}
+			else {
+				Insert (newnode, parent.right);
+			}
+		}	
+
+	}
+
+	public void Search(Node node){
+
+		/*Something has gone wrong*/
+		if (node == null) {
+			throw new Exception("ERROR: Search node null");
+
+		}
+		/*Perform the action*/
+		if (node.actdel!= null) {
+			node.actdel ();
+			return;
+		}
+
+		/*Perform decision*/
+		if (node.decdel() == true) {
+			Search(node.left);
+		} 
+		else {
+			 Search (node.right); 
+		}
+
+	}
+	
+}
