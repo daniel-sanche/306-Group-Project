a3c733607a0bac82c8cf032620fb4b048f02fd11
nickdaddy <nicholasb373@gmail.com>
2016-11-03 02:09:41 -0600

    Not sure if commits went through

--- 306-Game/Assets/Scripts/AItestScript.cs
+++ 306-Game/Assets/Scripts/AItestScript.cs
@@ -3,7 +3,14 @@ using System.Collections;
 
 public class AItestScript : MonoBehaviour {
 
+	public float aggrorange = 30f;
+
 	private DecisionTree ai;
+	private Node testleft = new Node();
+	private Node testright = new Node();
+	private Node testleftright = new Node();
+	private Vector2 mousepos;
+	private Vector2 myVector;
 
 	public object player;
 	void Awake(){
@@ -15,34 +22,60 @@ public class AItestScript : MonoBehaviour {
 
 		BuildDecisionTree ();
 
-		ai.Search (ai.root);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		mousepos = new Vector2 (Camera.main.ScreenToWorldPoint( Input.mousePosition).x, Camera.main.ScreenToWorldPoint( Input.mousePosition).y);
+		myVector = new Vector2 (transform.position.x, transform.position.y);
+		ai.Search (ai.root);
+		Debug.DrawRay (transform.position, mousepos- myVector, Color.red);
+
 	}
 
 	void BuildDecisionTree(){
 		ai.root.decdel = RangeCheck;
-		ai.root.left.actdel = wentLeft;
+		/*ai.root.left.actdel = wentLeft;
 		ai.root.right.actdel = wentRight;
-
+	*/
+		testleft.value = 25;
+		testleft.decdel = wentLeft;
+		testright.actdel = wentRight;
+		testright.value = 75;
+		testleftright.value = 30;
+		testleftright.actdel = wentLeftRight;
+		ai.Insert (testleft,ai.root);
+		ai.Insert (testright, ai.root);
+		ai.Insert (testleftright, ai.root);
 	}
 
-	public void wentLeft(){
+	public bool wentLeft(){
 		print ("Went Left\n");
+		return false;
+	}
+	public void wentLeftRight(){
+		print ("Went LeftRIght\n");
 	}
 
 	public void wentRight(){
 		print ("Went Right\n");
 	}
 
+
 	public bool RangeCheck(){
+		if (Vector2.Distance(mousepos, myVector) < aggrorange) {
+			Debug.Log ("In range");
+			return true;
+		}
+
+		return false;
+	}
+	public bool LungeDecider(){
 
 
 		int foo = Random.Range (0, 20);
-		if (foo > 10) {
+		if (foo > 20) {
 			return true;
 		}
 		return false;
--- 306-Game/Assets/Scripts/DecisionTree.cs
+++ 306-Game/Assets/Scripts/DecisionTree.cs
@@ -2,45 +2,67 @@
 using System.Collections;
 using System;
 
+public class Node{
+	public Node right;
+	public Node left;
+	public Decision decdel;
+	public Action actdel;
+
+	/* For Insertion*/
+	public int value;
+}
+
+public delegate bool Decision();
+public delegate void Action();
+
 
 public class DecisionTree : MonoBehaviour {
-	public delegate bool Decision();
-	public delegate void Action();
-
-	public class Node{
-		public Node right;
-		public Node left;
-		public Decision decdel;
-		public Action actdel;
-
-
-
-	}
-
+	
 
 
 	public Node root;
-
+	/* useful for inserting nodes*/
+	public int mid = 50;
 	/*Constructor*/
 	public  DecisionTree(){
+	
 		root = new Node();
+		root.value = mid;
 		root.right = new Node ();
 		root.left = new Node ();
 
-	
-
 	}
 
+	public void Insert(Node newnode, Node parent){
+		if (newnode == null || parent ==null) {
+			throw new Exception ("INSERT: newnode is null or parent is null");
+		}
 
+		if (parent.value > newnode.value) {
+			if (parent.left != null) {
+				parent.left = newnode;
+			} else {
+				Insert (newnode, parent.left);
+			}
+		}
+		else {
+			if (parent.right != null) {
+				parent.right = newnode;
+			}
+			else {
+				Insert (newnode, parent.right);
+			}
+		}	
+
+	}
 
 	public void Search(Node node){
 
 		/*Something has gone wrong*/
 		if (node == null) {
-			throw new Exception("I am an error");
+			throw new Exception("ERROR: Search node null");
 
 		}
-
 		/*Perform the action*/
 		if (node.actdel!= null) {
 			node.actdel ();
