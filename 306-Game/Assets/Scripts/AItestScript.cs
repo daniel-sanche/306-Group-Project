eaa6d05cd83dd0967f890bc62c1dcd77aa4ad5df
nickdaddy <nicholasb373@gmail.com>
2016-11-03 00:03:38 -0600

    Slowly realizing how to make AI

---
+++ 306-Game/Assets/Scripts/AItestScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AItestScript : MonoBehaviour {
+
+	private DecisionTree ai;
+
+	public object player;
+	void Awake(){
+		ai = new DecisionTree ();
+
+	}
+	// Use this for initialization
+	void Start () {
+
+		BuildDecisionTree ();
+
+		ai.Search (ai.root);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void BuildDecisionTree(){
+		ai.root.decdel = RangeCheck;
+		ai.root.left.actdel = wentLeft;
+		ai.root.right.actdel = wentRight;
+
+	}
+
+	public void wentLeft(){
+		print ("Went Left\n");
+	}
+
+	public void wentRight(){
+		print ("Went Right\n");
+	}
+
+	public bool RangeCheck(){
+
+
+		int foo = Random.Range (0, 20);
+		if (foo > 10) {
+			return true;
+		}
+		return false;
+	}		
+
+}
--- 306-Game/Assets/Scripts/DecisionTree.cs
+++ 306-Game/Assets/Scripts/DecisionTree.cs
@@ -1,18 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+
 
 public class DecisionTree : MonoBehaviour {
-    delegate void desdel();
-    delegate void actdel();
-    MyDelegate desdel;
-    MyDelegate actdel;
-	// Use this for initialization
-	void Start () {
+	public delegate bool Decision();
+	public delegate void Action();
+
+	public class Node{
+		public Node right;
+		public Node left;
+		public Decision decdel;
+		public Action actdel;
+
+
+
+	}
+
+
+
+	public Node root;
+
+	/*Constructor*/
+	public  DecisionTree(){
+		root = new Node();
+		root.right = new Node ();
+		root.left = new Node ();
+
 	
+
+	}
+
+
+
+	public void Search(Node node){
+
+		/*Something has gone wrong*/
+		if (node == null) {
+			throw new Exception("I am an error");
+
+		}
+
+		/*Perform the action*/
+		if (node.actdel!= null) {
+			node.actdel ();
+			return;
+		}
+
+		/*Perform decision*/
+		if (node.decdel()) {
+			Search(node.left);
+		} 
+		else {
+			 Search (node.right); 
+		}
+
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
