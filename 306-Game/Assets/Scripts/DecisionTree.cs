f2b0dec6b3d1679a1949bf817b0e71d10cb237ff
Nicholas Brunoro <nib851@tux7.usask.ca>
2016-11-02 18:03:57 -0600

    Starting DecisionTree

---
+++ 306-Game/Assets/Scripts/DecisionTree.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DecisionTree : MonoBehaviour {
+    delegate void desdel();
+    delegate void actdel();
+    MyDelegate desdel;
+    MyDelegate actdel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
