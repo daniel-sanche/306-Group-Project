cf242a15c85847dfab22cb398d12ccad634e6788
Daniel Sanche <d.sanche14@gmail.com>
2016-12-06 22:25:06 -0600

    fixed camera moving to player at start

--- 306-Game/Assets/Player/CameraFollow.cs
+++ 306-Game/Assets/Player/CameraFollow.cs
@@ -5,6 +5,8 @@ public class CameraFollow : MonoBehaviour {
 
 	//The smoothing speed of the follow
 	public float smoothingSpeed;
+
+	private float OffScreenDist = 10.0f;
 	
 	// Update is called once per frame
 	void Update () {
@@ -14,9 +16,14 @@ public class CameraFollow : MonoBehaviour {
 
 		//Calculate 2D distance between camera and player
 		float distance = Vector2.Distance (transform.position, player.transform.position);
+		float speed = smoothingSpeed;
+		if (distance > OffScreenDist) {
+			//if the player is significantly off screen (ex, at start), speed there instantly
+			speed = Mathf.Infinity;
+		}
 
 		//Creates vector that moves towards the player based on distance and smoothing speed
-		Vector2 moveTo = Vector2.MoveTowards (transform.position, player.transform.position, Time.deltaTime * distance * smoothingSpeed);
+		Vector2 moveTo = Vector2.MoveTowards (transform.position, player.transform.position, Time.deltaTime * distance * speed);
 
 		//Move GameObject to position
 		transform.position = new Vector3 (moveTo.x, moveTo.y, transform.position.z);
